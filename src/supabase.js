28875b6b022fd60536dba7bce52d06265f755b55
John Gould <johngould@Johns-MacBook-Air.local>
2026-02-24 15:07:08 -0700

    use env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://qzrokjgvrscbazldbypr.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6cm9ramd2cnNjYmF6bGRieXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NjE0MTIsImV4cCI6MjA4NzUzNzQxMn0.40hvKcu3MzaUK5QCgMpclSaxMLGAr7QHzkY7M-v3zuo'
-  ,{
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+  {
     auth: {
       detectSessionInUrl: true,
       persistSession: true,
